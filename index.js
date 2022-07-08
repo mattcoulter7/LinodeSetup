e683e65d3132eb90bbc9095b773bab5f119cc0f2
Matthew Coulter <mattcoul7@gmail.com>
2022-07-08 18:25:02 +1000

    Added https options

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var http = require('http');
 dotenv.config()
 const app = express();
 
-app.get('/colors',(req, res, next) => {
+app.get('/colors', (req, res, next) => {
     res.status(200).json([
         {
             color: "red",
@@ -40,5 +40,11 @@ app.get('/colors',(req, res, next) => {
 
 //const PORT = process.env.PORT || 5001
 //app.listen(PORT,() => console.log(`Server is running on this port ${PORT}`))
+
+var options = {
+    key: fs.readFileSync('/path/to/key.pem'),
+    cert: fs.readFileSync('/path/to/cert.pem'),
+    ca: fs.readFileSync('/path/to/ca.pem')
+};
 http.createServer(app).listen(80);
 https.createServer(options, app).listen(443);
